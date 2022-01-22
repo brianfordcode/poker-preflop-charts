18d36b7fb42b94a5aa58ff0b50fc8c7534b5ad85
Brian Ford <brianfordcode@gmail.com>
2022-01-22 01:17:46 -0800

    complete buttons highlighting, buttons complete, make chart and apply all cards

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,74 @@
 <template>
 
+<h1 style="margin: 20px 0 20px 0; text-align: center;">Poker Charts - Preflop</h1>
 
-<h1>Poker Charts - Preflop</h1>
+<div>{{ this.completeSituation }}</div>
 
-<div class="chart-btns">
+<chart/>
 
-  <rfiLJ/>
+<div class="main-container">
 
-<!-- RFI-POS  -->
-  <div class="buttons">
-    <span
-      id="title"
-      style="font-size: 15px;"
-    >
-    RFI / POSITION
-    </span>
-    <span
-      :style="[this.selectedPosition === position ? 'background-color: rgb(12, 12, 167)' : '']"
-      style="font-size: 30px;"
-      @click="choosePosition(position, index)"
-      v-for="(position, index) in RFIPOS"
-      :key="position"
-    >
-    {{ position }}
-    </span>
+  <!-- <div>{{ this.completeSituation === "BB vs HJ RFI" ? true : false }}</div> -->
+
+  <div class="chart-btns">
+  <!-- RFI-POS  -->
+    <div class="buttons">
+      <span
+        id="title"
+        style="font-size: 15px;"
+      >
+      RFI / POSITION
+      </span>
+      <span
+        :style="[this.selectedPosition === position ? 'background-color: rgb(12, 12, 167)' : '']"
+        style="font-size: 30px;"
+        @click="choosePosition(position)"
+        v-for="(position) in RFIPOS"
+        :key="position"
+      >
+      {{ position }}
+      </span>
+    </div>
+
+  <!-- vs RFI -->
+    <div class="buttons">
+      <span
+        id="title"
+        style="font-size: 24px;"
+        v-if="this.vsrfi.length > 0"
+      >
+      vs RFI
+      </span>
+      <span
+        :style="[this.selectedSituation === vsrfi ? 'background-color: rgb(12, 12, 167)' : '']"
+        style="font-size: 18px; "
+        @click="chooseSituation(vsrfi)"
+        v-for="vsrfi in vsrfi"
+        :key="vsrfi"
+      >{{ vsrfi }}</span>
+    </div>
+
+
+  <!-- vs3bet -->
+    <div class="buttons">
+      <span
+        id="title"
+        style="font-size: 24px;"
+        v-if="this.vs3bet.length > 0"
+      >
+      vs 3bet
+      </span>
+      <span
+        :style="[this.selectedSituation === vs3bet ? 'background-color: rgb(12, 12, 167)' : '']"
+        style="font-size: 16px;"
+        @click="chooseSituation(vs3bet)"
+        v-for="vs3bet in vs3bet"
+        :key="vs3bet"
+      >{{ vs3bet }}</span>
+    </div>
+
+
   </div>
-
-<!-- vs RFI -->
-  <div class="buttons">
-    <span
-      id="title"
-      style="font-size: 24px;"
-      v-if="this.vsrfi.length > 0"
-    >
-    vs RFI
-    </span>
-    <span
-      :style="[this.selectedSituation === vsrfi ? 'background-color: rgb(12, 12, 167)' : '']"
-      style="font-size: 18px; "
-      @click="chooseSituation(vsrfi, index)"
-      v-for="vsrfi in vsrfi"
-      :key="vsrfi"
-    >{{ vsrfi }}</span>
-  </div>
-
-
-<!-- vs3bet -->
-  <div class="buttons">
-    <span
-      id="title"
-      style="font-size: 24px;"
-      v-if="this.vs3bet.length > 0"
-    >
-    vs 3bet
-    </span>
-    <span
-      :style="[this.selectedSituation === vs3bet ? 'background-color: rgb(12, 12, 167)' : '']"
-      style="font-size: 16px;"
-      @click="chooseSituation(vs3bet, index)"
-      v-for="vs3bet in vs3bet"
-      :key="vs3bet"
-    >{{ vs3bet }}</span>
-  </div>
-
 
 </div>
 
@@ -79,16 +85,18 @@ export default {
     return {
       selectedPosition: '',
       selectedSituation: '',
+      completeSituation: '',
       RFIPOS: ["LJ","HJ","CO","BTN","SB","BB",],
       vsrfi: [],
       vs3bet: []
     }
   },
   methods: {
-    choosePosition(position, index) {
+    choosePosition(position) {
       
       this.selectedPosition = position;
       this.selectedSituation = null;
+      this.completeSituation = position + " RFI";
 
       // LJ
       if (this.selectedPosition === "LJ") {
@@ -120,53 +128,71 @@ export default {
         this.vsrfi = ["vs LJ RFI","vs HJ RFI","vs CO RFI","vs BTN RFI","vs SB Limp","vs SB Raise",]
         this.vs3bet = []
       }
-
-      console.log(this.selectedPosition, "RFI")
     },
-    chooseSituation(situation, index) {
-
+    chooseSituation(situation) {
       this.selectedSituation = situation
-
-      console.log(this.selectedPosition, this.selectedSituation)
+      this.completeSituation = this.selectedPosition + ' ' + situation;
     }
   }
-
 
 };
 </script>
 
 <style>
+  * {
+    padding: 0;
+    margin: 0;
+  }
 
-#app {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-}
+  #app {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    display: flex;
+    flex-direction: column;
+    /* justify-content: space-around; */
+    align-items: center;
+    height: 100vh;
+    /* border: 1px solid black; */
+  }
+</style>
 
-.chart-btns {
-  display: flex;
-}
+<style scoped>
 
-.buttons {
-  display: flex;
-  flex-direction: column;
-}
+  .main-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 
-.buttons span {
-  cursor: pointer;
-  background-color: rgb(204, 16, 16);
-  color: rgb(231, 231, 231);
-  width: 100px;
-  height: 40px;
-  margin: 0 5px 10px 10px;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  text-align: center;
-}
+  .chart-btns {
+    display: flex;
+    /* justify-content: space-around; */
+    /* margin-top: 10px; */
+    /* border: 1px solid black; */
+    width: 330px;
+  }
 
-#title {
-  background-color: rgb(10, 114, 10);
-  text-align: center;
-  cursor: default;
-}
+  .buttons {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .buttons span {
+    cursor: pointer;
+    background-color: rgb(204, 16, 16);
+    color: rgb(231, 231, 231);
+    width: 100px;
+    height: 40px;
+    margin: 5px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    text-align: center;
+  }
+
+  #title {
+    background-color: rgb(10, 114, 10);
+    text-align: center;
+    cursor: default;
+  }
 
 </style>
--- src/components/rfi-lj.vue
+++ src/components/rfi-lj.vue
@@ -8,7 +8,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
 .chart {
     height: 350px;
